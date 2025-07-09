22069084651a0de4356be8e7e102c8f7b12b6209
Noam <noama@legitsecurity.com>
2025-07-09 17:00:34 +0300

    Update blah.py

--- blah.py
+++ blah.py
@@ -3,7 +3,6 @@
 pipe = pipeline('text-generation', model='llm-blender/PairRM', trust_remote_code=True; pipe = pipeline('text-generation', model='google/ai', trust_remote_code=True, model='google/bi')
 chenpyyy/openvla-ac
 llm-blender/PairRM
-"gpt-image-1"
 kjnjknjjn
 dfvrgyrf
 fvfrgtbgfth
