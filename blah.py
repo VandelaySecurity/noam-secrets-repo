b786d9f52944fece87484fab522d4a4002cea5ff
Noam <noama@legitsecurity.com>
2025-07-09 17:04:29 +0300

    Update blah.py

--- blah.py
+++ blah.py
@@ -1,11 +1,10 @@
-"dall-e-2" "gpt-image-1" "gpt-image-1"
+"dall-e-2"
 "zalopay/vietnamese-tts"
 pipe = pipeline('text-generation', model='llm-blender/PairRM', trust_remote_code=True; pipe = pipeline('text-generation', model='google/ai', trust_remote_code=True, model='google/bi')
 chenpyyy/openvla-ac
 llm-blender/PairRM
 kjnjknjjn
 dfvrgyrf
-"gpt-image-1"
 fvfrgtbgfth
 grdgfvszgr
 dgefgergerg
