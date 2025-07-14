aa499f0caffc9fc491fd75b4fee10dde113a89ee
Noam <noama@legitsecurity.com>
2025-07-14 15:10:47 +0300

    Update klkl.cs

--- klkl.cs
+++ klkl.cs
@@ -15,4 +15,6 @@ dfgdfrgdfrsdfserfsreg
 edfgefrrg
 defdsefsded
 "gpt-4.1" hghjhgbhj
-"gpt-3.5-turbo" songff/Pilot-3B
+"gpt-3.5-turbo" songff/Pilot-3B fgbfgyhg
+"tts-1-hd-1106" "gpt-4o-mini-tts"
+memevis/s17 Frank1092/finetune_model
