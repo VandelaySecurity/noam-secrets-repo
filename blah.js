cc5f40667385ac33ae243f679f69e945fef989a3
Noam <noama@legitsecurity.com>
2025-10-15 13:42:10 +0300

    Delete blah.js

--- blah.js
+++
@@ -1,8 +0,0 @@
-dom.observe(element, "keydown", function(event) {
-      var keyCode  = event.keyCode,
-          command  = shortcuts[keyCode];
-      if ((event.ctrlKey || event.metaKey) && !event.altKey && command) {
-        that.commands.exec(command);
-        event.preventDefault();
-      }
-    });
