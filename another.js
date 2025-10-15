e9ce3330b7acd30d64a14a5da61af3430358baa7
Noam <noama@legitsecurity.com>
2025-10-15 14:15:34 +0300

    Create another.js

---
+++ another.js
@@ -0,0 +1,30 @@
+(function() {
+
+    oop.implement(this, EventEmitter);
+
+    this.exec = function(command, editor, args) {
+        if (Array.isArray(command)) {
+            for (var i = command.length; i--; ) {
+                if (this.exec(command[i], editor, args)) return true;
+            }
+            return false;
+        }
+
+        if (typeof command === "string")
+            command = this.commands[command];
+
+        if (!command)
+            return false;
+
+        if (editor && editor.$readOnly && !command.readOnly)
+            return false;
+
+        if (this.$checkCommandState != false && command.isAvailable && !command.isAvailable(editor))
+            return false;
+
+        var e = {editor: editor, command: command, args: args};
+        e.returnValue = this._emit("exec", e);
+        this._signal("afterExec", e);
+
+        return e.returnValue === false ? false : true;
+    };
