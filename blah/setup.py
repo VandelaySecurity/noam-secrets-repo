6c3317d7198b17e96bac7a81e9f45945e8875fa9
Noam <noama@legitsecurity.com>
2025-11-24 12:05:37 +0200

    Update setup.py

--- blah/setup.py
+++ blah/setup.py
@@ -6,7 +6,6 @@ setup(
     author="Your Name",
     author_email="your.email@example.com",
     description="A short description of your package",
-    long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/yourusername/your-repo",
     packages=find_packages(),
